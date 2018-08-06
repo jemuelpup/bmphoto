1a6f3b3572b0b0405a9c9a594acb0953e736de7f
jemuelpup <jemuelpup@gmail.com>
2018-08-06 19:37:48 +0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  steps: {}[] = [
+  	{
+  		title: "post your job",
+  		description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
+  	},{
+  		title: "get proposals",
+  		description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
+  	},{
+  		title: "get your job delivered",
+  		description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
+  	}
+  ];
+
+  ngOnInit() {
+  }
+
+}
