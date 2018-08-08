4705fd85ae6221e2db686c072db0a18af1bc8e23
jemuelpup <jemuelpup@gmail.com>
2018-08-08 21:09:42 +0800

    home page 80%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { FooterComponent,RoutingComponents } from './components/footer/footer.component';
+import { AppRoutingModule,RoutingComponents } from './/app-routing.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import {  } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faHeart);
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { FooterComponent,RoutingComponents } from './components/footer/footer.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,41 +1,196 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  steps: {}[] = [
-  	{
-  		title: "post your job",
-  		description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
-  	},{
-  		title: "get proposals",
-  		description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
-  	},{
-  		title: "get your job delivered",
-  		description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
-  	}
-  ];
+	steps: {}[] = [
+		{
+			title: "post your job",
+			description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
+		},{
+			title: "get proposals",
+			description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
+		},{
+			title: "get your job delivered",
+			description: "Less searching. More finding. Discover royalty-free images, illustrations and videos that will make you stand out."
+		}
+	];
 
-  todoList: {}[] = [
-    {
-      imgSrc: "assets/images/icon_02.png",
-      text: "Photography"
-    },{
-      imgSrc: "assets/images/icon_02.png",
-      text: "Photography"
-    },{
-      imgSrc: "assets/images/icon_02.png",
-      text: "Photography"
-    }
-  ];
+	todoList: {}[] = [
+		{
+			imgSrc: "assets/images/icon_02.png",
+			text: "Photography"
+		},{
+			imgSrc: "assets/images/icon_02.png",
+			text: "Photography"
+		},{
+			imgSrc: "assets/images/icon_02.png",
+			text: "Photography"
+		}
+	];
 
-  ngOnInit() {
-  }
+	galleryData: {}[] = [
+		{
+			sampleImg:"assets/images/gallery/galeryPic_01.jpg",
+			sampleImgAlt:"Description here",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			photographerName:"Ramon S.",
+			width:150,
+			height:200
+		},
+		{
+			sampleImg:"assets/images/gallery/galeryPic_02.jpg",
+			sampleImgAlt:"Description here",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			photographerName:"Ramon S."
+		},
+		{
+			sampleImg:"assets/images/gallery/galeryPic_03.jpg",
+			sampleImgAlt:"Description here",
+			photographerIcon:"assets/images/gallery/photographerIcon_01.jpg",
+			photographerName:"Ramon S."
+		},
+		{
+			sampleImg:"assets/images/gallery/galeryPic_04.jpg",
+			sampleImgAlt:"Description here",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			photographerName:"Ramon S."
+		},
+		{
+			sampleImg:"assets/images/gallery/galeryPic_05.jpg",
+			sampleImgAlt:"Description here",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			photographerName:"Ramon S."
+		},
+		{
+			sampleImg:"assets/images/gallery/galeryPic_06.jpg",
+			sampleImgAlt:"Description here",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			photographerName:"Ramon S."
+		}
+	];
+	// photographersPricingPackages
+	/*///reserved data
+	ppp: {} = {
+		photographerName: "Shannel Stewart",
+		photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+		price: 260,
+		packageDesc: "Lorem Ipsum is simply dummy text of the prin. . .",
+		featuredImages: [
+			{
+				imageName:"Dummy",
+				source:"assets/images/photographerFeaturedImages/samplePic_01.jpg",
+			},
+			{
+				imageName:"Dummy",
+				source:"assets/images/photographerFeaturedImages/samplePic_02.jpg",
+			},
+			{
+				imageName:"Dummy",
+				source:"assets/images/photographerFeaturedImages/samplePic_03.jpg",
+			}
+		],
+		packageLink: "",
+		followers: 4479
+	}
+	/**/
+	photographerPricingPakages: {}[] = [];
+
+	ngOnInit() {
+		this.photographerPricingPakages.push({
+			photographerName: "Shannel Stewart",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			price: 260,
+			packageDesc: "Lorem Ipsum is simply dummy text of the prin. . .",
+			featuredImages: [
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_01.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_02.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_03.jpg",
+				}
+			],
+			packageLink: "",
+			followers: 4479
+		});
+		this.photographerPricingPakages.push({
+			photographerName: "Shannel Stewart",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			price: 1780,
+			packageDesc: "Lorem Ipsum is simply dummy text of the prin. . .",
+			featuredImages: [
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_04.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_05.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_06.jpg",
+				}
+			],
+			packageLink: "",
+			followers: 4479
+		});
+		this.photographerPricingPakages.push({
+			photographerName: "Shannel Stewart",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			price: 9.70,
+			packageDesc: "Lorem Ipsum is simply dummy text of the prin. . .",
+			featuredImages: [
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_07.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_08.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_09.jpg",
+				}
+			],
+			packageLink: "",
+			followers: 4479
+		});
+		this.photographerPricingPakages.push({
+			photographerName: "Shannel Stewart",
+			photographerIcon:"assets/images/photographerIcon/photographerIcon_01.jpg",
+			price: 3480,
+			packageDesc: "Lorem Ipsum is simply dummy text of the prin. . .",
+			featuredImages: [
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_10.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_11.jpg",
+				},
+				{
+					imageName:"Dummy",
+					source:"assets/images/photographerFeaturedImages/samplePic_12.jpg",
+				}
+			],
+			packageLink: "",
+			followers: 4479
+		});
+	}
 
 }
