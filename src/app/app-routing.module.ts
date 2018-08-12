e904c5829024b7e1d9311a8052df148c6ac47d3b
jemuel elimanco <jemuel elimanco>
2018-08-12 13:40:23 +0800

    Registration page ongoing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { VisitorProfileComponent } from './components/visitor-profile/visitor-profile.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
-	{ path: 'home', component: HomeComponent }
+	{ path: 'home', component: HomeComponent },
+	{ path: 'visitorprofile', component: VisitorProfileComponent },
+	{ path: 'registration', component: RegistrationComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes,{ useHash: true }) ],
 	exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
 export const RoutingComponents = [
-	HomeComponent
+	HomeComponent,
+	VisitorProfileComponent
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule,RoutingComponents } from './/app-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
-
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 // ngx bootstrap
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars, faCamera, faImages, faPaintBrush } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { VisitorProfileComponent } from './components/visitor-profile/visitor-profile.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 library.add(faHeart,faBars, faCamera, faImages, faPaintBrush);
@@ -22,7 +24,9 @@ library.add(faHeart,faBars, faCamera, faImages, faPaintBrush);
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    VisitorProfileComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +34,7 @@ library.add(faHeart,faBars, faCamera, faImages, faPaintBrush);
     FontAwesomeModule,
     BsDropdownModule.forRoot()
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
